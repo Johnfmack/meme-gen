41a833152c264cae070856e8c7687ac14cded082
John mackay <jmackay101@gmail.com>
2020-10-19 15:42:02 +1100

    create meme gen

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
+import Header from './Header'; 
+import MemeGenerator from './MemeGenerator';
+import './style.css';
 import './App.css';
 
 function App() {
   return (
+/**
+ * Other modern/advanced React features/topics to learn:
+ * 
+ * Official React Context API - https://reactjs.org/docs/context.html
+ * Error Boundaries - https://reactjs.org/docs/error-boundaries.html
+ * render props - https://reactjs.org/docs/render-props.html
+ * Higher Order Components - https://reactjs.org/docs/higher-order-components.html
+ * React Router - https://reacttraining.com/react-router/core/guides/philosophy
+ * React Hooks - https://reactjs.org/docs/hooks-intro.html
+ * React lazy, memo, and Suspense - https://reactjs.org/blog/2018/10/23/react-v-16-6.html
+ */
+    
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+      <Header/> 
+      <h1>john's meme gen title</h1>
+      <MemeGenerator />
+         
     </div>
   );
 }
---
+++ src/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './style.css';
+function Header(){
+    return (
+        <header>
+            <img src="http://www.pngall.com/wp-content/uploads/2016/05/Trollface.png"
+            alt='problem?'></img>
+       <p>meme generator</p>
+        </header>
+    )
+
+}
+
+
+export default Header;
